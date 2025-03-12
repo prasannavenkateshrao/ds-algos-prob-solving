3d3de503bd9bba79b849ed164fd10c3d51687eda
Prasanna Prahaladhan <prasanna.prahaladhan@zoominfo.com>
2025-03-12 15:42:01 +0530

    updated palindrome and string reverse

--- ds-problems/palindrome.js
+++ ds-problems/palindrome.js
@@ -16,3 +16,19 @@ console.log(isStringPalindrome);
 
 let val = 'stringdata';
 console.log(val[0]);
+
+var palindromeWithChars = function(s) {
+    var cleanedString = s.toLowerCase().replace(/[^a-z0-9]/g,'');
+    let left = 0, right = cleanedString.length - 1;
+    while(left < right){
+        if(cleanedString[left] !== cleanedString[right]) return false;
+        left ++;
+        right --;
+    }
+    return true;
+}
+console.log(palindromeWithChars("A man, a plan, a canal: Panama"));
+console.log(palindromeWithChars("race a car"));
+console.log(palindromeWithChars("No lemon, no melon"));
+console.log(palindromeWithChars("Was it a car or a cat I saw?"));
+console.log(palindromeWithChars("hello!"));
